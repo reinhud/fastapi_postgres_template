9b87d2bd059ff7443ea48bd005180b8e22c35d15
Christina Frezynski <christina@frez.me>
2024-10-01 15:29:07 -0500

    cleanup base filter

--- fastapi_server/app/api/filters/base.py
+++ fastapi_server/app/api/filters/base.py
@@ -6,14 +6,10 @@ from fastapi_filter.contrib.sqlalchemy import Filter as SQLAlchemyFilter
 class BaseFilter(SQLAlchemyFilter):
     id: uuid.UUID | None = None
     id__in: list[uuid.UUID] | None = None
-    created_at: datetime | None = None
-    created_at__gt: datetime | None = None
-    created_at__lt: datetime | None = None
     created_at__gte: datetime | None = None
     created_at__lte: datetime | None = None
-    updated_at: datetime | None = None
-    updated_at__gt: datetime | None = None
-    updated_at__lt: datetime | None = None
     updated_at__gte: datetime | None = None
     updated_at__lte: datetime | None = None
     
+    # Default sorting by updated_at with newest first
+    order_by: list[str] | None = ['-updated_at']
